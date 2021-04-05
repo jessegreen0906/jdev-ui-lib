3740714c16e2c8e2b080508b4ee120559d9b34dc
jessegreen0906 <jessegreen0906@gmail.com>
2021-04-05 13:14:54 +1000

    Fixing GH actions errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import {BasicTemplate} from "./pageTemplates/Basic/Basic";
 import {Button} from "./components/Button/Button";
+import {Footer} from "./components/Footer/Footer";
 
 module.exports = {
 	BasicTemplate: BasicTemplate,
-    Button: Button
+    Button: Button,
+	Footer: Footer
 }
