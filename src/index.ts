b99af39e1b6214eba3f50f3092f7827bf8352551
Jesse Green <jessegreen0906@gmail.com>
2021-06-07 18:23:10 +1000

    feature: Updating build pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import {BasicTemplate} from "./pageTemplates/Basic/Basic";
+import {PageTemplate} from "./pageTemplates/PageTemplate/PageTemplate";
 import {Button} from "./components/Button/Button";
 import {Footer} from "./components/Footer/Footer";
+import {Heading} from "./components/Heading/Heading";
+import {InputLabel} from "./components/InputLabel/InputLabel";
+import {Link} from "./components/Link/Link";
+import {Paragraph} from "./components/Paragraph/Paragraph";
+import {SiteMenu} from "./components/SiteMenu/SiteMenu";
 
 module.exports = {
 	BasicTemplate: BasicTemplate,
+	PageTemplate: PageTemplate,
     Button: Button,
-	Footer: Footer
+	Footer: Footer,
+	Heading: Heading,
+	InputLabel: InputLabel,
+	Link: Link,
+	Paragraph: Paragraph,
+	SiteMenu: SiteMenu
 }
